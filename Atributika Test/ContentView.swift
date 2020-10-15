7805923d4c6eca494a18fcb59cf2818cd243a6a9
Danny Buonocore <danBuonocore@gmail.com>
2020-10-15 15:24:51 -0400

    Initial Commit

---
+++ Atributika Test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Atributika Test
+//
+//  Created by Danny Buonocore on 10/15/20.
+//  Copyright © 2020 Live Coin Watch. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
